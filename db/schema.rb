c801b6f87ab1d20b13b684bff1728281ee862c41
Yuji Monkawa <monkawa0208@gmail.com>
2025-10-14 14:01:15 +0900

    カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_09_085627) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_14_045830) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -47,7 +47,27 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_09_085627) do
     t.text "response"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content_ja"
+    t.text "ai_translate"
     t.index ["posted_on"], name: "index_entries_on_posted_on"
     t.index ["user_id", "posted_on"], name: "index_entries_on_user_id_and_posted_on", unique: true
     t.index ["user_id"], name: "index_entries_on_user_id"
   end
+
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "entries", "users"
+end
