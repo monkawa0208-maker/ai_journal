42938eb5cae58536c6047d4a67872340e30e54a8
Yuji Monkawa <monkawa0208@gmail.com>
2025-10-19 19:15:39 +0900

    home/index.html.erbをトップページに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "entries#index"
+  root "home#index"
+  
   resources :entries do
     post :generate_feedback, on: :member
     post :translate, on: :collection
